7dcda5e9d6a27118c8817692cef424397a98542f
rickherrera3001 <rihr.3001@gmail.com>
2024-05-13 18:52:14 -0600

    Initial Commit

---
+++ DiscoverAirports/DiscoverAirportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverAirportsApp.swift
+//  DiscoverAirports
+//
+//  Created by Ricardo Developer on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverAirportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
